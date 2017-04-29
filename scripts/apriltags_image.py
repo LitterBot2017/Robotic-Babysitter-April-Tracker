385b9d11644d9c3bfd20ffcffa2158e514ce631e
Yoga Nadaraajan <yyn@ada.personalrobotics.ri.cmu.edu>
2017-04-29 16:51:14 -0400

    Final code

--- scripts/apriltags_image.py
+++ scripts/apriltags_image.py
@@ -7,6 +7,8 @@ from cv_bridge import CvBridge, CvBridgeError
 from sensor_msgs.msg import Image
 from std_msgs.msg import Float32
 from math import atan2
+from math import sqrt
+from april_tracker.msg import pose
 
 import pdb
 
@@ -30,7 +32,7 @@ class AprilTagMarker:
     self.marked_image = []
     self.pub_image = 0
 
-    self.angle_pub = rospy.Publisher("/angle", Float32, queue_size = 1)
+    self.pose_pub = rospy.Publisher("/pose", pose, queue_size = 1)
 
     # Initialize detection subscriber
     self.detection_sub = rospy.Subscriber("/apriltags/detections",AprilTagDetections,self.detectionCb)
@@ -40,6 +42,8 @@ class AprilTagMarker:
   # Grab detection result, published from apriltag identifier
   def detectionCb(self,data):
     self.detection_result = data
+    self.drawMarker(data)
+    self.drawArrow(data)
 
   # Grab image and convert from Image message to openCV image
   def imageCb(self,data):
@@ -56,32 +60,71 @@ class AprilTagMarker:
       print(e)    
 
   # Draw marker on the center of the apriltag
-  def drawMarker(self):
+  def drawMarker(self, data):
     if len(self.detection_result.detections) == 0:
       return -1
     ########################
     ### INSERT CODE HERE ###
     ########################
-    corners=self.detection_result.detections[0] 
-    centerx=(corners.corners2d[0].x+corners.corners2d[1].x)/2
-    centery=(corners.corners2d[0].y+corners.corners2d[3].y)/2
-    centery=(corners.corners2d[0].y+corners.corners2d[3].y)/2
-    centerz=(corners.corners2d[0].z+corners.corners2d[2].z)/2
-    x = int(centerx/centerz)
-    y = int(centery/centerz)
-    cv2.circle(self.image,(x,y),10,(0,0,255),-1)
-    self.angle_motion(x,y)
+    for i in range(len(self.detection_result.detections)):
+      print i
+      corners=self.detection_result.detections[i] 
+      centerx=(corners.corners2d[0].x+corners.corners2d[1].x)/2
+      centery=(corners.corners2d[0].y+corners.corners2d[3].y)/2
+      centery=(corners.corners2d[0].y+corners.corners2d[3].y)/2
+      centerz=(corners.corners2d[0].z+corners.corners2d[2].z)/2
+      x = int(centerx/centerz)
+      y = int(centery/centerz)
+      cv2.circle(self.image,(x,y),10,(0,0,255),-1)
     self.publishImage()
 
-  def angle_motion(self,x,y):
+  def drawArrow(self,data):
+    if len(data.detections) == 0:
+      return -1
+    tag0 = ()
+    tag1 = ()
+    tag0_set = False
+    tag1_set = False
+    for i in range(len(data.detections)):
+      tag = data.detections[i]
+      if(tag.id == 2):
+        tag0_set = True
+        centerx=(tag.corners2d[0].x+tag.corners2d[1].x)/2
+        centery=(tag.corners2d[0].y+tag.corners2d[3].y)/2
+        centery=(tag.corners2d[0].y+tag.corners2d[3].y)/2
+        centerz=(tag.corners2d[0].z+tag.corners2d[2].z)/2
+        # Transform to arm base frame
+        centerx = centerx - 100
+        centery = centery - 86
+        tag0x = int(centerx/centerz)
+        tag0y = int(centery/centerz)
+        tag0 = (tag0x,tag0y)
+      if(tag.id == 1):
+        tag1_set = True
+        centerx=(tag.corners2d[0].x+tag.corners2d[1].x)/2
+        centery=(tag.corners2d[0].y+tag.corners2d[3].y)/2
+        centery=(tag.corners2d[0].y+tag.corners2d[3].y)/2
+        centerz=(tag.corners2d[0].z+tag.corners2d[2].z)/2
+        tag1x = int(centerx/centerz)
+        tag1y = int(centery/centerz)
+        tag1 = (tag1x,tag1y)
+    if tag0_set and tag1_set:
+      #cv2.circle(self.image,tag1,20,(0,255,0),-1)
+      cv2.line(self.image, tag0, tag1,(255,0,0),5)
+      self.angle_motion(tag0, tag1)
+    self.publishImage()
+
+  def angle_motion(self,arm,anki):
     self.counter += 1
-    if(self.counter%10): 
-      diff_x = x-self.old_x
-      diff_y = y-self.old_y
-      val = atan2(diff_y,diff_x)
-      self.old_x = x
-      self.old_y = y
-      self.angle_pub.publish(val)
+    if(self.counter): 
+      diff_x = anki[0] - arm[0]#x-self.old_x
+      diff_y = anki[1] - arm[1]#y-self.old_y
+      val = atan2(diff_x,diff_y)
+      dist = sqrt((diff_x*diff_x)+(diff_y*diff_y))
+      msg = pose()
+      msg.angle = val
+      msg.dist = dist
+      self.pose_pub.publish(msg)
 
 if __name__ == '__main__':
 
@@ -91,7 +134,7 @@ if __name__ == '__main__':
   marker = AprilTagMarker()
   
   # Spin ROS
-  rate = rospy.Rate(120) #hz
+  rate = rospy.Rate(10) #hz
   while not rospy.is_shutdown(): 
-    marker.drawMarker()
+    
     rate.sleep()
